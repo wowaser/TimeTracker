258864b79960d91071c32a09b71307b403501b5d
Vladimir Vechtomov <vladimir.vechtomov@tuni.fi>
2021-03-06 07:11:53 +0200

    Subclassed color buttons

--- tracker/colorbutton.cpp
+++ tracker/colorbutton.cpp
@@ -1,9 +1,38 @@
 #include "colorbutton.h"
+#include <QPainter>
+#include <QDebug>
 
-ColorButton::ColorButton(){
+ColorButton::ColorButton(QWidget*&){
     setAutoFillBackground(true);
 }
+/*
+void ColorButton::paintEvent(QPaintEvent *e){
 
+    //QPainter painter(this);
+    //painter.fillRect(this->rect(), btn_color);
+
+
+    QPushButton::paintEvent(e);
+}
+*/
+void ColorButton::mousePressEvent(QMouseEvent *event){
+    QColor clr = this->palette().button().color();
+    emit pressed(clr);
+    QPushButton::mousePressEvent(event);
+}
+
+//  SLOT
+void ColorButton::set_color(QColor &clr){
+    QPalette pal = palette();
+    pal.setColor(QPalette::Button, clr);
+    setAutoFillBackground(true);
+    setPalette(pal);
+    update();
+    emit selected_clr_changed(clr);
+}
+
+
+/*
 void ColorButton::enterEvent(QEvent *){
        QPalette pal = palette();
        pal.setColor(QPalette::Button, QColor(Qt::blue));
@@ -17,3 +46,4 @@ void ColorButton::leaveEvent(QEvent *){
     setPalette(pal);
     update();
 }
+*/
